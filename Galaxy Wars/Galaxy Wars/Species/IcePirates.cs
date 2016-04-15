47bac174b3783b63a7087ed60b5006f4ef2cb654
TJB III <thomasbuida@gmail.com>
2016-04-15 14:08:47 -0500

    Initial setup of folders and example class/interfaces

---
+++ Galaxy Wars/Galaxy Wars/Species/IcePirates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars.Species
+{
+    class IcePirates : Species, IWarMonger
+    {
+        public double aggressionMultiplier { get; set; }
+        public string alignment { get; set; }
+        public string type { get; set; }
+        public int vanquishedEars { get; set; }
+
+        public void Berserker(Species Enemy)
+        {
+            Enemy.population -= 10000;
+            this.population -= 2500;
+        }
+
+        public bool hasEyePatch { get; set; }
+        public bool hasIceHook { get; set; }
+        public bool foundTreasure { get; set; }
+        public bool literacy { get; set; }
+        public int peglegs { get; set; }
+
+        public IcePirates()
+        {
+            this.hasEyePatch = true;
+            this.foundTreasure = false;
+            this.vanquishedEars = 0;
+            this.type = "Warmonger";
+        }
+    }
+}
---
+++ Galaxy Wars/Galaxy Wars/Vehicles/VehicleType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars.Vehicles
+{
+    class VehicleType
+    {
+        public int speed { get; set; }
+        public double defense { get; set; }
+        public double offense { get; set; }
+
+        public virtual void Fly()
+        {
+            Console.WriteLine("Currently flying");
+        }
+
+        public VehicleType()
+        {
+            this.speed = 100;
+            this.defense = 10;
+            this.offense = 25;
+        }
+    }
+}
