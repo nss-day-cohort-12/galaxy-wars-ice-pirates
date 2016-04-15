3af18e82dd3d96e3beca7077f92df2615b9fed38
Brian Doane <Brian Doane>
2016-04-15 16:00:01 -0500

    Parched class, interface and vehicle initial setup

---
+++ Galaxy Wars/Galaxy Wars/Species/Interfaces/IReligion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars.Species
+{
+    interface IReligion
+    {
+        string type { get; set; }
+        void Convert(Species Enemy);
+        void Indoctrine(Species Enemy);
+        string commandment { get; set; }
+        double zealotMultiplier { get; set; }
+    }
+}
