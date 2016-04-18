21d62a1fe1f8f96df75788034d75bb49275e8c85
Michael <mbennette85@gmail.com>
2016-04-18 09:05:14 -0500

    hydrologist stuff

---
+++ Galaxy Wars/Galaxy Wars/Species/Hydrologists.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars.Species
+{
+    class Hydrologists: Species 
+    {
+        public bool pHD { get; set; }
+        public bool athletic { get; set; }
+        public bool hasBeaker { get; set; }
+        public bool hasTechnology { get; set; }
+        public int significant { get; set; }
+
+        public void clone (Species Enemy)
+        {
+            this.population += 5000;
+        }
+        public double intelligenceMultiplier { get; set; }
+        public string alignment { get; set; }
+        public string type { get; set; }
+
+        public void replenish (Species Enemy)
+        {
+            this.population += 10000;
+            Enemy.population -= 1000;
+        }
+
+    }
+}
